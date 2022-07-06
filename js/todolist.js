59cb03befcafc784a49043fbc733e99543850d9c
CarolLau13 <825827342@qq.com>
2022-07-06 22:05:12 +0800

    add todolist

--- js/todolist.js
+++ js/todolist.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
     let listItem = $('#list-item');
     let listItemContent = $('#list-item .content');
     let toDoListNum = $('#toDoListNum');
+    let doneListNum = $('#doneListNum');
 
     let clickCount = 0;
-    let i = 0;
+    let clickCount2 = 0;
+    // let i = 0;
+    // let j = 0;
 
     inputContent.keypress(function (e) {
         // console.log(e.keyCode);
@@ -17,15 +20,17 @@ $(document).ready(function () {
             }
 
             let $div = $('<div class="list-item" id="list-item"></div>');
-            $div.attr('id', 'listItem' + i);
-            i = i + 1;
+            // $div.attr('id', 'listItem' + i);
+            // i = i + 1;
 
             $div.append('<li class="rect"></li>');
-            $div.append('<input type="checkbox" name="" id="">');
 
-            let content = $('<li class="content"></li>');
-            content.html(inputContent.val());
-            $div.append(content);
+            let $checkbox = $('<input type="checkbox" name="" id="">');
+            $div.append($checkbox);
+
+            $content = $('<li class="content"></li>');
+            $content.html(inputContent.val());
+            $div.append($content);
 
             let $image = $('<img src="./images/circle.png" alt="">');
             // $image.attr('class', 'image' + i);
@@ -35,12 +40,74 @@ $(document).ready(function () {
             inputContent.val('');
             clickCount += 1;
             toDoListNum.html(clickCount);
-
+            // 圆形删除键添加点击事件
             $image.on('click', function (e) {
                 // console.log($(e.target));
                 $(e.target).parent().remove();
                 clickCount -= 1;
                 toDoListNum.html(clickCount);
+            })
+
+            // checkbox添加点击事件
+            $checkbox.on('click', function (e) {
+                $(e.target).parent().remove();
+                clickCount -= 1;
+                toDoListNum.html(clickCount);
+
+                let $divDone = $('<div class="list-item done-list-item" id="done-list-item">');
+
+                $divDone.append('<li class="rect"></li>');
+
+                let $checkbox2 = $('<input type="checkbox" name="" id="" checked>');
+                $divDone.append($checkbox2);
+
+                let $content = $('<li class="content"></li>');
+                let contentChecked = $(e.target).next().html();
+                $content.html(contentChecked);
+                $divDone.append($content);
+
+                let $image = $('<img src="./images/circle.png" alt="">');
+                $divDone.append($image);
+
+                $('.done').prepend($divDone);
+                clickCount2 += 1;
+                doneListNum.html(clickCount2);
+
+                // 添加点击事件(删除done-list-item)
+                $image.on('click', function (e) {
+                    $(e.target).parent().remove();
+                    clickCount2 -= 1;
+                    doneListNum.html(clickCount2);
+                })
+
+                // 添加点击事件($checkbox2)
+                $checkbox2.on('click', function (e) {
+                    $(e.target).removeAttr('checked');
+                    $(e.target).parent().remove();
+                    clickCount2 -= 1;
+                    doneListNum.html(clickCount2);
+
+                    // 把待办事项放回list-item
+                    clickCount += 1;
+                    toDoListNum.html(clickCount);
+
+                    let $div = $('<div class="list-item" id="list-item"></div>');
+
+                    $div.append('<li class="rect"></li>');
+
+                    let $checkbox = $('<input type="checkbox" name="" id="">');
+                    $div.append($checkbox);
+
+                    $content = $('<li class="content"></li>');
+                    let contentUnchecked = $(e.target).next().html();
+                    $content.html(contentUnchecked);
+                    $div.append($content);
+
+                    let $image = $('<img src="./images/circle.png" alt="">');
+                    $div.append($image);
+
+                    $('.onGoing').prepend($div);
+                })
             })
 
             // if (clickCount == 0) {
